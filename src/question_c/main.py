8590512195cc047922c88c9ae09691bcaa67e774
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-16 04:58:21 +0000

    question c

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,16 @@
 #add import
+
+from src.question_a.question_a import get_person_category
+
+def main():
+    
+    while True:
+        age = int(input("Enter a person's age (or type 'q' to quit): "))
+        category = get_person_category(age)      
+        print(f"This person is a(n) {category}.")
+        user_input = input("Type 'q' to quit or any other key to continue: ")
+        if user_input == 'q':
+                print("Thank you for using the program. Goodbye!")
+               
+  
+        
