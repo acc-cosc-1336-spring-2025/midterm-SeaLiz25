839325d04d361b6c937f6a7192bb02183c20a0b2
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-16 05:32:35 +0000

    Question 3

--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -9,8 +9,8 @@ class Test_Config(unittest.TestCase):
     def test_question_a_config(self):
         self.assertEqual(True, test_config())
 
-        def test_get_miles_per_hour(self):
-            self.assertEqual(19.883872, test_get_miles_per_hour(32,60))
+print(reverse_string("hello world"))
+
           
 
 
