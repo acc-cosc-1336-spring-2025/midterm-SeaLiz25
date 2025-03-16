045cb6029aab76d4f763ac70daaa9b13c2e3aebf
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-16 05:04:56 +0000

    question a 1,2,3

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,17 +1,10 @@
 #write functions here, don't add input('') statements here!
 
-import unittest
-
-#follow this example to add questions b, c, and d for testing including their functions
-from src.question_a.question_a import test_config
-
-class Test_Config(unittest.TestCase):
-
-    def test_question_a_config(self):
-        self.assertEqual(True, test_config())
-
-        def test_get_person_category(self):
-            self.assertEqual("child", test_get_person_category(2))
-            self.assertEqual("teenager", test_get_person_category(14))
-            self.assertEqual("adult",test_get_person_category(20))
-            self.assertEqual("infant", test_get_person_category(1))
+def get_miles_per_hour(kilometers, minutes):
+    if kilometers < 0 or minutes <= 0:
+        return "Invalid arguments"
+    
+    hours = minutes / 60
+    miles = kilometers * 0.621371
+    mph = miles / hours
+    return mph
