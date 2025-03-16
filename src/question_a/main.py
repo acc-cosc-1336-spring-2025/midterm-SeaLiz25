b6ccaecc6d42b80dcd4c524201f889c3da126b03
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-16 04:23:26 +0000

    question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,11 @@
 #add import
+
+from src.question_a.question_a import get_person_category
+
+def main():              
+    age = int(input("Enter a person's age:  "))
+    category = get_person_category(age)
+
+    print ("This person is a(n) ", category)
+
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,19 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def get_person_category(age):
+
+    if age < 0:
+        return "invalid number"
+    elif age <= 1:
+        return "infant"
+    elif age <= 13:
+        return "child"
+    elif age <20:
+        return "teenager"
+    elif age <= 125:
+        return "adult"
+    else:
+        return "invalid number"    
+    
