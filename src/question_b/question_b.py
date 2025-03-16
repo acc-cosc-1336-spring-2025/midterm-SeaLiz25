c4bc175ec97ed1613d24bb23eb50c88c1c1db259
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-16 04:51:10 +0000

    question b

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,17 @@
 #write functions here, don't add input('') statements here!
+
+import unittest
+
+#follow this example to add questions b, c, and d for testing including their functions
+from src.question_a.question_a import test_config
+
+class Test_Config(unittest.TestCase):
+
+    def test_question_a_config(self):
+        self.assertEqual(True, test_config())
+
+        def test_get_person_category(self):
+            self.assertEqual("child", test_get_person_category(2))
+            self.assertEqual("teenager", test_get_person_category(14))
+            self.assertEqual("adult",test_get_person_category(20))
+            self.assertEqual("infant", test_get_person_category(1))
